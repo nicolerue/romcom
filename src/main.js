03829ac7ff81d8b5959955033337cd716d25793b
Johann Dee <johanndee1994@gmail.com>
2023-07-14 22:27:04 -0700

    Clean up and deleteSavedCover

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 // Create variables targetting the relevant DOM elements here 👇
-//ALL BUTTONS NAV BAR
+// ALL BUTTONS NAV BAR
 var showNewBtn = document.querySelector(".random-cover-button");
 var makeOwnCoverBtn = document.querySelector(".make-new-button");
 var homeBtn = document.querySelector(".home-button");
 var saveCoverBtn = document.querySelector(".save-cover-button");
 var viewSavedBtn = document.querySelector(".view-saved-button");
 
-//BUTTON - MAKE YOUR OWN COVER
+// BUTTON - MAKE YOUR OWN COVER
 var makeMyBookBtn = document.querySelector(".create-new-book-button");
 
-//ALL PAGES
+// ALL PAGES
 var homePage = document.querySelector(".home-view");
 var makeOwnCoverPage = document.querySelector(".form-view");
 var viewSavedCoversPage = document.querySelector(".saved-view");
@@ -22,22 +22,20 @@ var mainCoverTagline = document.querySelector(".tagline");
 var mainCoverTagline1 = document.querySelector(".tagline-1");
 var mainCoverTagline2 = document.querySelector(".tagline-2");
 
-//Form Input Fields
+// Form Input Fields
 var coverInputField = document.querySelector(".user-cover");
 var titleInputField = document.querySelector(".user-title");
 var desc1InputField = document.querySelector(".user-desc1");
 var desc2InputField = document.querySelector(".user-desc2");
 
-//SAVED COVERS SECTION
+// SAVED COVERS SECTION
 var savedCoversSection = document.querySelector(".saved-covers-section");
+
 // We've provided a few variables below
 var savedCovers = [];
 
 // Add your event listeners here 👇
-
-window.addEventListener("DOMContentLoaded", function () {
-  createRandom();
-});
+window.addEventListener("load", createRandom);
 
 showNewBtn.addEventListener("click", createRandom);
 
@@ -47,14 +45,16 @@ viewSavedBtn.addEventListener("click", clickedViewSavedCover);
 
 homeBtn.addEventListener("click", clickedHomeButton);
 
-makeMyBookBtn.addEventListener("click", createnewCoverObj);
+makeMyBookBtn.addEventListener("click", createNewCoverObj);
 
 saveCoverBtn.addEventListener("click", saveNewCover);
 
+savedCoversSection.addEventListener("dblclick", deleteSavedCover);
+
 
 // Create your event handlers and other functions here 👇
-//function for saving the cover
 
+// function for saving the cover
 function saveNewCover(event) {
   event.preventDefault();
   var currentCover = {
@@ -66,8 +66,8 @@ function saveNewCover(event) {
   };
 
   var isDuplicate = false;
-  // check if the current cover already exists in the array
 
+  // check if the current cover already exists in the array
   for (var i = 0; i < savedCovers.length; i++) {
     if (
       savedCovers[i].coverImg === currentCover.coverImg &&
@@ -83,28 +83,28 @@ function saveNewCover(event) {
   }
 }
 
-for (var i = 0; i < savedCovers.length; i++) {
-  if (
-    savedCovers[i].coverImg === currentCover.coverImg &&
-    savedCovers[i].title == currentCover.title &&
-    savedCovers[i].tagline1 == currentCover.tagline1 &&
-    savedCovers[i].tagline2 == currentCover.tagline2
-  ) {
-    savedCovers.splice(i, 1);
-  }
-}
+// for (var i = 0; i < savedCovers.length; i++) {
+//   if (
+//     savedCovers[i].coverImg === currentCover.coverImg &&
+//     savedCovers[i].title == currentCover.title &&
+//     savedCovers[i].tagline1 == currentCover.tagline1 &&
+//     savedCovers[i].tagline2 == currentCover.tagline2
+//   ) {
+//     savedCovers.splice(i, 1);
+//   }
+// }
 
-viewSavedCoversPage.addEventListener("click", (element) => {
-  for (var i = 0; i < savedCovers.length; i++) {
-    if (savedCovers[i].id == element.target.id) {
-      savedCovers.splice(i, 1);
-    }
-  }
-  clickedViewSavedCover();
-});
+// viewSavedCoversPage.addEventListener("click", (element)=> {
+//   for (var i = 0; i < savedCovers.length; i++) {
+//     if (savedCovers[i].id == element.target.id) {
+//       savedCovers.splice(i, 1);
+//     }
+//   }
+//   clickedViewSavedCover();
+// });
 
 //function for creating a new cover obj
-function createnewCoverObj(event) {
+function createNewCoverObj(event) {
   event.preventDefault();
   var newUserCover = createCover(
     coverInputField.value,
@@ -125,8 +125,7 @@ function createnewCoverObj(event) {
   clickedHomeButton();
 }
 
-// Functions for toggling between buttons
-
+//functions for toggling between buttons
 function showElementOrPage(element) {
   element.classList.remove("hidden");
 }
@@ -162,7 +161,7 @@ function clickedViewSavedCover() {
 
   var newHTML = `<section class="saved-view saved-covers-section">`; //
   for (var i = 0; i < savedCovers.length; i++) {
-    newHTML += `<section class="mini-cover">
+    newHTML += `<section class="mini-cover mini-cover-icons">
     <img class="mini-cover" src="${savedCovers[i].coverImg}" />
     <h2 class="cover-title">${savedCovers[i].title}</h2>
     <h3 class="tagline">
@@ -179,18 +178,16 @@ function clickedViewSavedCover() {
 
 function clickedHomeButton() {
   checkPage(homePage);
-  removeElementOrPage(homeBtn);
   showElementOrPage(showNewBtn);
   showElementOrPage(saveCoverBtn);
-  removeElementOrPage(makeOwnCoverPage);
+  removeElementOrPage(homeBtn);
   removeElementOrPage(viewSavedCoversPage);
+  removeElementOrPage(makeOwnCoverPage);
   removeElementOrPage(savedCoversSection);
-  removeElementOrPage(miniDisplayIcons);
 
 }
 
-
-// creating random funcctions
+//creating random cover
 function createRandom() {
   var randIndexCovers = getRandomIndex(covers);
   var randIndexTitles = getRandomIndex(titles);
@@ -207,7 +204,7 @@ function createRandom() {
   mainCoverTagline2.innerText = randDesc2;
 }
 
-// creating a random number
+//creating a random number
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
@@ -222,4 +219,15 @@ function createCover(imgSrc, title, descriptor1, descriptor2) {
     tagline2: descriptor2,
   };
   return cover;
+}
+
+//delete saved cover
+function deleteSavedCover(event) {
+  var coverToDelete = event.target.parentElement.id;
+  for (var i = 0; i < savedCovers.length; i++) {
+    if (coverToDelete == savedCovers[i].id) {
+      savedCovers.splice(i, 1);
+    }
+  }
+  clickedViewSavedCover();
 }
